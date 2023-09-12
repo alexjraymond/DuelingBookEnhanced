9181180e15bb480d11214e508bcb730efe3c43f3
Alex Raymond <alexjacobraymond@gmail.com>
2023-09-11 21:27:02 -0700

    perfected thunk

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,21 +1,17 @@
-
-
-
 window.onload = function () {
-  
-  const thunk = document.getElementById('think_btn')
-  document.addEventListener('keydown', (e) => {
-    e.preventDefault();
+  const thunk = document.getElementById('think_btn');
 
-    const handler = "KeyT"
-    
-    if (handler) {
-      thunk?.click();
-      return;
+  document.addEventListener('keydown', (e) => {
+    const handler = e.key.toLowerCase();
+    console.log('Key pressed:', handler);
+  
+    if (!(e.target instanceof HTMLInputElement)) {
+      if (handler === 't') {
+        console.log('Clicking "think_btn"');
+        thunk?.click();
+      }
     }
-    
-    console.log('Pressed a key without a handler.')
-});
-  console.log(thunk)
+  });
+
   console.log('Page has been reloaded');
 };
