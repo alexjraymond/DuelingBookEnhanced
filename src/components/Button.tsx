0b387845089d1fbf384d130e91473c6f428a2dfa
Alex Raymond <alexjacobraymond@gmail.com>
2023-09-16 21:09:45 -0700

    biggest push ever - components, styling popup

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import React, { ButtonHTMLAttributes } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    buttonText: string;
+    buttonUrl: string;
+}
+
+const Button: React.FC<ButtonProps> = ({buttonText, buttonUrl, ...buttonProps }) => {
+    const buttonLink = () => {
+        window.open(buttonUrl, '_blank');
+    };
+
+
+return (
+    <div>
+        <button onClick={buttonLink} {...buttonProps}>{buttonText}</button>
+    </div>
+    );
+};
+
+export default Button;
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import Button from "./components/Button";
 
 const Popup = () => {
   const [disableAllOptions, setDisableAllOptions] = useState(false);
@@ -7,90 +8,104 @@ const Popup = () => {
   const [immediatelyGoToMainMenu, setImmediatelyGoToMainMenu] = useState(false);
   const [isNightMode, setIsNightMode] = useState(false);
 
-  const handleFeedbackLinkClick = () => {
-    // google form feedback logic... window.open?
-  };
 
   const handleSettingsButtonClick = () => {
     // settings redirect... window.location.href, or react router?
   };
 
-  const toggleNightMode = () => {
-    setIsNightMode(!isNightMode);
-    // night mode logic - probably just styles to make the text white and background black
-  };
+
+      // night mode logic - probably just styles to make the text white and background black
+    // here's the spans and divs and stuff
+    // card text left side: div w/ id "preview_txt"
+    // chatbox: class="cout_txt textarea scrollpane selectable" (theres no id for it its cringe)
+    // good color for night mode background: #242428
+    const toggleNightMode = () => {
+      console.log('youre clicking nightmode');
+      const textBoxes = document.querySelectorAll(".os_viewport");
+      console.log(textBoxes);
+      textBoxes.forEach((textBox) => {
+        if (textBox instanceof HTMLElement) {
+          console.log('yay we dark mode now');
+          textBox.style.backgroundColor = "#242428";
+        }
+      });
+      setIsNightMode(!isNightMode);
+    };
 
   return (
-    <div 
-      style={{
-        display: 'flex', 
-        flexDirection: 'column',
-      }}
-      >
-      <h1
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: '#FFA987',
-          color: '#1E1E24',
-          marginTop: 0,
-          marginBottom: 0
-        }}
-      
-      >Settings</h1>
-      <div 
-      id="input_container"
-      style={{
-        padding: 5,
-        display: 'grid',
-        justifyItems: 'start',
-        gap: 10,
-        
-      }}
-      >
-      <label>
-        <input
-          type="checkbox"
-          checked={disableAllOptions}
-          onChange={() => setDisableAllOptions(!disableAllOptions)}
-        />
-        Disable/Enable All Options
-      </label>
-
-      <label>
-        <input
-          type="checkbox"
-          checked={skipIntro}
-          onChange={() => setSkipIntro(!skipIntro)}
-        />
-        Skip Intro
-      </label>
-
-      <label>
-        <input
-          type="checkbox"
-          checked={immediatelyGoToMainMenu}
-          onChange={() => setImmediatelyGoToMainMenu(!immediatelyGoToMainMenu)}
-
-        />
-        Go to Settings
-      </label>
-
-      <label>
-        <input
-          type="checkbox"
-          checked={isNightMode}
-          onChange={toggleNightMode}
-        />
-        Night Mode
-      </label>
-      <div id= "button-container">
-        <button onClick={handleFeedbackLinkClick}>Provide Feedback</button>
-        <button onClick={handleSettingsButtonClick}>Go to All Settings</button>
-      </div>
-      </div>
+    <div
+  style={{
+    display: 'flex',
+    flexDirection: 'column',
+  }}
+>
+  <h1
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: '#F7EBE8',
+      marginTop: 0,
+      marginBottom: 0,
+    }}
+  >
+    Settings
+  </h1>
+  <div
+    id="input_container"
+    style={{
+      padding: 5,
+      display: 'flex',
+      flexDirection: 'column', // Change to column layout
+      gap: 10,
+    }}
+  >
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <input
+        id="allOptions"
+        type="checkbox"
+        checked={disableAllOptions}
+        onChange={() => setDisableAllOptions(!disableAllOptions)}
+      />
+      <label htmlFor="allOptions">Disable/Enable All Options</label>
     </div>
+
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <input
+        id="skipIntro"
+        type="checkbox"
+        checked={skipIntro}
+        onChange={() => setSkipIntro(!skipIntro)}
+      />
+      <label htmlFor="skipIntro">Skip Intro</label>
+    </div>
+
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <input
+        id="goToSettings"
+        type="checkbox"
+        checked={immediatelyGoToMainMenu}
+        onChange={() => setImmediatelyGoToMainMenu(!immediatelyGoToMainMenu)}
+      />
+      <label htmlFor="goToSettings">Go to Settings</label>
+    </div>
+
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <input
+        id="nightMode"
+        type="checkbox"
+        checked={isNightMode}
+        onChange={toggleNightMode}
+      />
+      <label htmlFor="nightMode">Night Mode</label>
+    </div>
+
+    <div id="button-container">
+      <Button buttonText='Bugs & Feedback' buttonUrl='https://docs.google.com/forms/d/e/1FAIpQLSfBB8Llqu-VmJeGDQKjGO2gtw9tSe7pOCUijSFn13eAZCaqew/viewform?vc=0&c=0&w=1&flr=0' />
+      <Button buttonText={'Open DB'} buttonUrl='http://www.DuelingBook.com/html5' />
+    </div>
+  </div>
+</div>
   );
 };
 
