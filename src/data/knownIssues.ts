02c2c9e92ebfc43c7d8afd5a7269cf653d1ad2b9
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2023-10-17 20:13:00 +0000

    added known issue of multiple actions to the same hotkey

--- src/data/knownIssues.ts
+++ src/data/knownIssues.ts
@@ -7,4 +7,8 @@ export const knownIssues = [
     issue: "Changed Settings Don't Immediately Take Effect",
     description: "If you change settings while already on duelingbook.com, you need to reload the page for some settings to update.",
   },
+  {
+    issue: "Multiple Actions For the Same Hotkey",
+    description: "Currently, users can set multiple actions to the same hotkey for buggy effects. Will be disabled soon. ",
+  },
 ]
