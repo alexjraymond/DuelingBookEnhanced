18d850457da681517ed86403ade5bf29bc349858
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2023-11-07 00:04:57 +0000

    fixed responsiveness with coming soon component to calculate how many cards need to be rendered

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import card from "../assets/images/dbe_logo.png"
+import React, { useState, useEffect } from 'react';
+import card from "../assets/images/dbe_logo.png";
 
-const ComingSoon = () => {
+const ComingSoon: React.FC = () => {
+  const calculateMaxCardsInRow = () => Math.floor(window.innerWidth / 275);
+  const [cardsToRender, setCardsToRender] = useState(Math.min(5, calculateMaxCardsInRow()));
+
+  useEffect(() => {
+    const handleResize = () => {
+      setCardsToRender(Math.min(5, calculateMaxCardsInRow()));
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const cardWord = cardsToRender === 1 ? 'card' : 'cards';
+
   return (
     <>
-        <div className="flex justify-center">
-          <img src={card} />
-          <img src={card} />
-          <img src={card} />
-          <img src={card} />
-          <img src={card} />
-        </div>
-        <div>
-          <h1 className='text-2xl justify-center flex'>We set 5 cards face down and will be back with this section soon...</h1>
-        </div>
-      </>
-  )
+      <div className="flex justify-center flex-wrap">
+        {Array.from({ length: cardsToRender }, (_, index) => (
+          <img key={index} src={card} alt="Card" className="m-2" style={{ width: '128px', height: '128px' }} />
+        ))}
+      </div>
+      <div>
+        <h1 className='text-2xl justify-center flex'>We set {cardsToRender} {cardWord} face down and will be back with this section soon...</h1>
+      </div>
+    </>
+  );
 }
 
-export default ComingSoon
+export default ComingSoon;
