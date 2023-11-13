9ddd208a883b15f08c7ea134a2451c2db70885c8
Alex Raymond <alexjacobraymond@gmail.com>
2023-11-12 16:51:28 -0800

    the new tab works

--- src/background.ts
+++ src/background.ts
@@ -6,7 +6,7 @@ polling();
 
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === "install" || details.reason === "update") {
-    chrome.tabs.create({url: 'chrome-extension://cbjnilkbeknnlohlemgbeaebaedokcbb/new-features.html'})
+    chrome.tabs.create({url: 'chrome-extension://cbjnilkbeknnlohlemgbeaebaedokcbb/NewFeatures.html'})
     console.log("new tab firing");
   } 
 
