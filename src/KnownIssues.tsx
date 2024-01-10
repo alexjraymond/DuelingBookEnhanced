28a2a6e19c490f6f63c899e4641871dd1cbdecd7
Nic Shepard <nShepard204@gmail.com>
2024-01-10 17:51:51 -0500

    Did a thing wooo

--- src/KnownIssues.tsx
+++ src/KnownIssues.tsx
@@ -4,29 +4,30 @@ import { knownIssues } from './data/knownIssues'
 import { plannedFeatures } from './data/plannedFeatures'
 
 const KnownIssues = () => {
-    return (
-        <>
-            <div className='container justify-center mb-4'>
-                <h1 className='text-2xl text-center font-bold bg-gray-200 rounded-lg mb-4'>Known Issues</h1>
-                {knownIssues.map((knownIssue) => (
-                    <Issue key={knownIssue.issue} issue={knownIssue.issue} description={knownIssue.description} />
-                ))}
-            </div>
-            <div className='container justify-center mb-4'>
-                <h1 className='text-2xl text-center font-bold bg-gray-200 rounded-lg mb-4'>Future Plans</h1>
-                <ul className='max-w-md space-y-1 list-disc list-inside'>
-                    {plannedFeatures.map((plannedFeature) => (
-                        <li
-                            key={plannedFeature.feature}
-                            className={plannedFeature.completed ? 'line-through' : ''}
-                        >
-                            {plannedFeature.feature}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </>
-    )
+  return (
+    <>
+      {/* Known Issues */}
+      <div className='container justify-center mb-4'>
+        <h1 className='text-2xl text-center font-bold bg-gray-200 rounded-lg mb-4'>Known Issues</h1>
+        To check where we're at in development, and to see the issues we're aware of, <a href="https://github.com/users/alexjraymond/projects/5/views/1" target='_blank'>click here to see our project roadmap!</a>
+      </div>
+
+      {/* Future Plans */}
+      <div className='container justify-center mb-4'>
+        <h1 className='text-2xl text-center font-bold bg-gray-200 rounded-lg mb-4'>Future Plans</h1>
+        <ul className='max-w-md space-y-1 list-disc list-inside'>
+          {plannedFeatures.map((plannedFeature) => (
+            <li
+              key={plannedFeature.feature}
+              className={plannedFeature.completed ? 'line-through' : ''}
+            >
+              {plannedFeature.feature}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
 }
 
 export default KnownIssues
